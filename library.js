3bdaf399a436f3d9a81847440477cccdd0afa696
unknown <gabelwarega@gmail.com>
2025-05-26 14:40:18 +0300

    Fixed the object method dynamic creation

--- library.js
+++ library.js
@@ -1,3 +1,5 @@
+const myLibrary = [];
+
 // create a constructor object
 class Book{
     constructor(title, author,pages,status){
@@ -6,17 +8,18 @@ class Book{
         this.title = title;
         this.author = author;
         this.pages = pages;
-        this.status = status;
+        this.status = status
     }
-
 }
-// create an object instance
-const books = new Book('Atomic Habits','Musk',345,'Read')
-
-// script for adding/create a book grabbing the parameters of the object
-function addBookToLibrary(){
-
+// The method serves to create objects dynmically and store them on the go
+function addBookToLibrary(title, author, pages,status){
+    const createBook = new Book(title, author,pages,status) // create object
+    myLibrary.push(createBook) // add objects created to the array.
+    return createBook
 }
-const myLibrary = [];
-myLibrary.push(books)
-console.log(myLibrary)
+
+// let me create some objects to test this functionliity
+
+//console.log(addBookToLibrary('Marco','Gab', 56,true))
+addBookToLibrary('Jerry','Mark',57,false)
+
